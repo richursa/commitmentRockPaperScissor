e777505dd2fde48681dd09c9f62eb68936fdb229
richursa <richursa@gmail.com>
2018-10-18 19:47:11 +0530

    made changes

--- alice.py
+++ alice.py
@@ -1,8 +1,41 @@
+import hashlib
 import socket
+print("waiting for bob to connect")
 serverSocket = socket.socket()
 serverSocket.bind(("localhost",9999))
 serverSocket.listen(5)
+
 connection , address = serverSocket.accept()
-print("bob connected from" + address)
-secretKey = raw_input("enter a secret key")
-choice = raw_input("enter either rock , paper or scissors")
+print("bob connected from " ,address)
+secretKey = raw_input("enter a secret key ")
+aliceChoice = raw_input("enter either rock , paper or scissor ")
+string = secretKey + aliceChoice
+hash = hashlib.sha256(string)
+connection.send(hash.hexdigest())
+print("bob is choosing ")
+bobChoice = connection.recv(1024)
+print("bob has chosen ")
+print(bobChoice)
+connection.send(secretKey)
+connection.send(aliceChoice)
+if bobChoice == "rock":
+    if aliceChoice == bobChoice :
+        print("draw")
+    elif aliceChoice == "scissor" :
+        print("you lost")
+    else:
+        print("you won")
+elif bobChoice == "scissor":
+    if aliceChoice == bobChoice:
+        print("draw")
+    elif aliceChoice == "paper":
+        print("you lost")
+    else:
+        print("you won")
+else:
+    if aliceChoice == bobChoice:
+        print("draw")
+    elif aliceChoice == "rock":
+        print("you lost")
+    else:
+        print("you won")
--- bob.py
+++ bob.py
@@ -1,13 +1,38 @@
 import hashlib
 import socket
 clientSocket = socket.socket()
-serverIP = raw_input("enter alices's ip address  to connect")
-port = input("enter port no")
+serverIP = raw_input("enter alices's ip address  to connect  ")
+port = input("enter port no  ")
 clientSocket.connect((serverIP,port))
-print("connected successfully")
-msg = clientSocket.recv(102400)
+print("connected successfully ")
+aliceCommitment = clientSocket.recv(102400)
 print("alice has commited ")
-print(msg)
-choice = raw_input("enter either rock paper or scissor")
-clientSocket.send(choice)
-
+print(aliceCommitment)
+bobChoice = raw_input("enter either rock,  paper or scissor ")
+clientSocket.send(bobChoice)
+aliceSecret  = clientSocket.recv(102400)
+aliceChoice  = clientSocket.recv(102400)
+if(hashlib.sha256(aliceSecret+aliceChoice).hexdigest() == aliceCommitment):
+    if bobChoice == "rock":
+        if aliceChoice == bobChoice :
+            print("draw")
+        elif aliceChoice == "scissor" :
+            print("you won")
+        else:
+            print("you lost")
+    elif bobChoice == "scissor":
+        if aliceChoice == bobChoice:
+            print("draw")
+        elif aliceChoice == "paper":
+            print("you won")
+        else:
+            print("you lost")
+    else:
+        if aliceChoice == bobChoice:
+            print("draw")
+        elif aliceChoice == "rock":
+            print("you won")
+        else:
+            print("you lost")
+else:
+    print("alice has cheated ")
